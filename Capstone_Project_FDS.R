ef501e6741edc61af4de2a52e00a92a7a30b3d48
bonjih <37001472+bonjih@users.noreply.github.com>
2020-01-21 19:32:27 +1000

    updated main code

--- Capstone_Project_FDS.R
+++ Capstone_Project_FDS.R
@@ -1,9 +1,10 @@
-library(recipes)
+library(ggbiplot)
+library(scales)
 library(dplyr)
+library(recipes)
 library(caret)
 library(devtools)
 library(ggplot2)
-library(ggbiplot)
 library(tidyverse)
 library(tseries)
 library(naniar)
@@ -12,12 +13,19 @@ library(ggfortify)
 library(imputeTS)
 library(lubridate)
 library(magrittr)
-library(data.table)
+library(rlang)
+library(Hmisc)
+library(latticeExtra)
+library(factoextra)
+library(lattice)
+library(FactoMineR)
+library(psych)
+library(GPArotation)
 
 #rm(list = ls()) 
 
 dataset <- read.csv("data/ESMdata.csv", header = TRUE)
-newDS <- na.interpolation(dataset)# Impute the missing values with  na.interpolation
+newDS <- na_interpolation(dataset)# Impute the missing values with  na.interpolation
 df <- data.frame(newDS)
 options(stringsAsFactors = FALSE)
 
@@ -33,10 +41,53 @@ ggplot(dfts, aes(x = date, y = dep)) +
                date_breaks = "2.5 weeks") +
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
-#PCA
-mood.pca <- df[c(11:30)]
-dfCors <- cor(mood.pca[ ,1:20], method="spearman") # convert to a value between 0 & 1
-df.pca <- prcomp(dfCors [,c(1:20)], center = TRUE, scale. = TRUE) 
-summary(df.pca)
-ggbiplot(df.pca, labels = rownames(dfCors))
+#PCA setup
+mood <- df[c(2,3, 5,11:30)]
+mood <- mood %>% group_by(date) %>% summarise_at(vars(phase:se_handle), mean) 
+moodDaily <- mood[c(4:22)]
+
+rownames(mood) = make.names(mood$phase, unique=TRUE )
+
+#moodCors <- cor(moodDaily, method="spearman") # convert to a value between 0 & 1
+moodDaily.pca <- prcomp(moodDaily, center = TRUE, scale. = TRUE)
+
+moodDaily.pca$rotation
+
+#PCA Eigenvalue
+screeplot(moodDaily.pca, type = "l", npcs = 5, main = "Screeplot of the first 5 PCs")
+abline(h = 1, col="red", lty=5)
+legend("topright", legend=c("Eigenvalue = 1"),
+       col=c("red"), lty=5, cex=0.6)
+
+plot(moodDaily.pca$x[,1],moodDaily.pca$x[,2], xlab="PC1 (60.6%)", ylab = "PC2 (11.4%)", main = "PC1 / PC2 - plot")
+ 
+#summary(moodDaily.pca)
+
+#PCA Plot
+#ggbiplot::ggbiplot(moodDaily.pca, labels = rownames(mood), 
+                   #circle = TRUE, var.scale = 1 ,
+                   #obs.scale = 1, varname.size = 4) 
+                   #ggtitle("PCA of Patient moods - 238 obs")  
+              
+biplot(moodDaily.pca, col=c("black","red"), cex=c(0.7,0.8))
+
+fviz_pca_biplot(moodDaily.pca, geom.ind = "point", pointshape = 21, 
+             pointsize = 2, 
+             fill.ind = as.factor(mood$phase),
+             col.ind = "black", 
+             palette = "jco", 
+             addEllipses = TRUE,
+             label = "var",
+             col.var = "black",
+             repel = TRUE,
+             legend.title = "Test Phase") +
+  ggtitle("PCA plot Patient mood v test Phase") +
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+
+
+
+
+
 
