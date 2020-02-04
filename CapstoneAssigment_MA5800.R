7efe82e162c87061697ea98655f4e687150ca662
bonjih <37001472+bonjih@users.noreply.github.com>
2020-02-04 21:06:10 +1000

    Update CapstoneAssigment_MA5800.R
    mostly fixed to normal

--- CapstoneAssigment_MA5800.R
+++ CapstoneAssigment_MA5800.R
@@ -21,6 +21,7 @@ library(lattice)
 library(FactoMineR)
 library(psych)
 library(GPArotation)
+library(cluster)
 
 
 rm(list = ls()) 
@@ -30,18 +31,6 @@ dataset <- read.csv("Data/ESMdata.csv", header = TRUE)
 newDS <- na_interpolation(dataset)# Impute the missing values with na.interpolation
 df <- data.frame(newDS)
 options(stringsAsFactors = FALSE)
-
-#Time series for 'dep' results
-dfts <- df %>%
-  mutate(date = as.Date(date, format="%d/%m/%Y")) %>%
-  complete(date = full_seq(date, period = 1)) 
-
-ggplot(dfts, aes(x = date, y = dep)) +
-  geom_line() +
-  scale_x_date(name = "Date (239 days)", 
-               date_labels = "%d-%m-%Y", 
-               date_breaks = "2.5 weeks") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 #################################
 
@@ -56,33 +45,39 @@ text(x=1:length(cd)+2, y=cd, labels=ifelse(cd>4*mean(cd, na.rm=T),names(cd),""),
 influential <- as.numeric(names(cd)[(cd > 4*mean(cd, na.rm=T))]) 
 infRmDf <- df[-c(1, 21, 41), ]
 
-#EFA to fine factors
+#Time series for 'dep' results
+dfts <- df %>%
+  mutate(date = as.Date(date, format="%d/%m/%Y")) %>%
+  complete(date = full_seq(date, period = 1)) 
 
-efa_cor <- cor(infRmDf[c(11:30)], method = 'spearman')
-efa_cor = as.data.frame(t(efa_cor))
-efa_cor$groups <- NA
-efa_cor$RowNumber <- seq.int(nrow(efa_cor))
+ggplot(dfts, aes(x = date, y = dep)) +
+  geom_line() +
+  scale_x_date(name = "Date (239 days)", 
+               date_labels = "%d-%m-%Y", 
+               date_breaks = "2.5 weeks") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+####################
+#Corrolation matrix
+
+fa_cor <- cor(infRmDf[c(11:30)], method = "spearman")
+fa_cor <- sqrt(fa_cor^2) #try for normality
+fa_cor = as.data.frame(t(fa_cor))
+fa_cor$groups <- NA
+fa_cor$RowNumber <- seq.int(nrow(fa_cor))
 
 ####################
 #Assumption Test  
-
-#cortest.bartlett(df[c(11:30)] , n=1476) (Snedecor et al 1989)
-#out -> Bartlett's test is highly significant, x2(190) = 23547, p < .001 - (Cerny & Kaiser 1977, pp.43-47)
+#cortest.bartlett(df[c(11:30)] , n=1476) #(Snedecor et al 1989)
+#out -> Bartlett's test is significant, x2(190) = 23547, p < .001 - (Cerny & Kaiser 1977, pp.43-47)
 # and therefore factor analysis is appropriate.
 #KMO(r = df[, c(11:30)]) #Overall MSA =  0.95 
 #fa.parallel(df[,c(11:30)],fm="pa") #quasi 3
 #alpha(df[c(11:30)], check.keys=TRUE) 
 #Cronbach alpha, >0.94 - (Yurdugül, H, 2008)
-
+#det(fa_cor)
+#which(fa_cor > 0.8)
 ####################
-
-efaGroups <- principal(efa_cor[c(1:20)], nfactors=3, rotate="oblimin", scores = TRUE)
-efa.groups <- print.psych(efaGroups , cut = 0.4, sort = TRUE, scores = TRUE)
-
-#Assign groups based on efaGroups
-efa_cor[efa_cor$RowNumber %in% c(16, 12, 17, 20, 9, 4, 11, 7, 19, 18), "groups"] <- "Mood Positive"
-efa_cor[efa_cor$RowNumber %in% c(5, 6, 10, 2, 15, 8), "groups"] <- "Mood Negitive"
-efa_cor[efa_cor$RowNumber %in% c(3, 14, 13,1), "groups"] <- "Mood Uncertain"
 
 #use EFA and kmeans confirm number of gruops
 #kmeans - optimal groups -3
@@ -98,33 +93,51 @@ fviz_cluster(groups, data = DataStandardised )  +
  
 #################################
 
+faGroups <- principal(df[c(11:30)], nfactors=3, rotate="oblimin", scores = TRUE)
+fa.groups <- print.psych(faGroups , cut = 0.4, sort = TRUE)
+fa.diagram(faGroups)
+
+#residuals view 
+residuals <- factor.residuals(fa_cor[c(1:20)], faGroups$loadings)
+residuals <- as.matrix(residuals[upper.tri(residuals)])
+large.resid <- abs(residuals) > 0.05
+sum(large.resid)/nrow(residuals)
+sqrt(mean(residuals^2))
+hist(residuals)
+
+#Assign groups based on efaGroups
+fa_cor[fa_cor$RowNumber %in% c(16, 12, 20, 9, 17, 11, 4, 7, 19), "groups"] <- "Mood Positive"
+fa_cor[fa_cor$RowNumber %in% c(5, 10, 2, 15, 6, 8, 18), "groups"] <- "Mood Negitive"
+fa_cor[fa_cor$RowNumber %in% c(3, 14, 13,1), "groups"] <- "Mood Uncertain"
+
+#Eigenvalue >1
+plot(faGroups$values, type = "b")
+abline(h = 1, col="red", lty=5)
+legend("topright", legend=c("Eigenvalue = 1"),
+       col=c("red"), lty=5, cex=0.6)
+
+
+#########################
+
 #PCA setup
 #mood <- df[c(2,3, 5,11:30)]
 #mood <- mood %>% group_by(date) %>% summarise_at(vars(phase:se_handle), mean) 
 #moodDaily <- mood[c(4:22)] 
 
-#########################
-
-moodefa_cor.pca <- prcomp(efa_cor[c(1:20)], center = TRUE) # includes data standardisation
-
-#ggbiplot::ggbiplot(moodefa_cor.pca, ellipse=TRUE, labels=rownames(efa_cor), groups=efa_cor$groups)  
+moodfa_cor.pca <- prcomp(fa_cor[c(1:20)], center = TRUE) # for fviz_pca_biplot / includes data standardisation
+moodfa_cor.pca$x
+#ggbiplot::ggbiplot(moodfa_cor.pca, ellipse=TRUE, labels=rownames(fa_cor), groups=fa_cor$groups)  
 
 #moodefa_cor.pca$rotation
 
-#PCA Eigenvalue
-screeplot(moodefa_cor.pca, type = "l", npcs = 5, main = "Screeplot of the first 5 PCs")
-abline(h = 1, col="red", lty=5)
-legend("topright", legend=c("Eigenvalue = 1"),
-       col=c("red"), lty=5, cex=0.6)
-
 #par(mfrow=c(2,2))
  
-#summary(moodefa_cor.pca)
+#summary(moodfa_cor.pca)
 
 #PCA Plot
-fviz_pca_biplot(moodefa_cor.pca, geom.ind = "point", pointshape = 21, 
+fviz_pca_biplot(moodfa_cor.pca, geom.ind = "point", pointshape = 21, 
              pointsize = 2, 
-             fill.ind = efa_cor$groups,
+             fill.ind = fa_cor$groups,
              col.ind = "black", 
              palette = "jco", 
              addEllipses = TRUE,
@@ -136,9 +149,14 @@ fviz_pca_biplot(moodefa_cor.pca, geom.ind = "point", pointshape = 21,
   theme(plot.title = element_text(hjust = 0.5))
 
 
+#########################
+
+#split dataset into phases,
 
 
-
+Mood.Positive <- mean(c(16, 12, 17, 20, 9, 4, 11, 7, 19, 18))
+Mood.Negitive <- mean(c(5, 6, 10, 2, 15, 8))
+Mood.Uncertain <- mean(c(3, 14, 13,1))
 
 
 
