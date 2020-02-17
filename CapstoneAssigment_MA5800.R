417b4c7f3147f0a852830eda1e33f8793d59ddbd
bonjih <37001472+bonjih@users.noreply.github.com>
2020-02-17 16:36:59 +1000

    added DFA component window
    added DFA component window

--- CapstoneAssigment_MA5800.R
+++ CapstoneAssigment_MA5800.R
@@ -1,65 +1,65 @@
-library(ggbiplot)
 library(scales)
 library(dplyr)
-library(recipes)
 library(caret)
 library(devtools)
 library(ggplot2)
 library(tidyverse)
 library(tseries)
-library(naniar)
 library(reshape2)
-library(ggfortify)
 library(imputeTS)
-library(lubridate)
-library(magrittr)
-library(rlang)
-library(Hmisc)
-library(latticeExtra)
-library(factoextra)
-library(lattice)
-library(FactoMineR)
 library(psych)
+library(Rtsne)
 library(GPArotation)
 library(cluster)
-
+library(nonlinearTseries)
 
 rm(list = ls()) 
 
 dataset <- read.csv("Data/ESMdata.csv", header = TRUE)
+dataset$dayno <- dataset$dayno[order(dataset$dayno)]
+dataset$dayno <- as.numeric(factor(dataset$dayno)) # re factor 'dayno' due to gaps in 'dayno'
 
 newDS <- na_interpolation(dataset)# Impute the missing values with na.interpolation
 df <- data.frame(newDS)
 options(stringsAsFactors = FALSE)
-
+df <- df %>% group_by(dayno)
 #################################
 
-#detecting, removing outliers
+#detecting, removing influential outliers
 outlierdf <- (df[c(11:30)])
 #outlier viz cook dist
 cd <- cooks.distance(lm(outlierdf))
-plot(cd,pch=1, cex = 1)
-abline(h = 4*mean(cd,na.rm = T), col = "blue")
-text(x=1:length(cd)+2, y=cd, labels=ifelse(cd>4*mean(cd, na.rm=T),names(cd),""), col="blue")
-#remove
+cddf <- as.data.frame(cd)
+cddf$index <- seq.int(nrow(cddf))
+
+ggplot(cddf, aes(x = index, y = cd)) +
+   geom_point(color="red") +
+   geom_hline(yintercept = 4*mean(cd), color="blue") 
+
+#removed influentials
 influential <- as.numeric(names(cd)[(cd > 4*mean(cd, na.rm=T))]) 
-infRmDf <- df[-c(1, 21, 41), ]
+infRmDf <- df[-influential, ]
 
-#Time series for 'dep' results
-dfts <- df %>%
-  mutate(date = as.Date(date, format="%d/%m/%Y")) %>%
-  complete(date = full_seq(date, period = 1)) 
+#reduce n dimenssions to 2 for ggplot (there are better packages than ggplot for PCA/EFA etc)
+tsne.pca = Rtsne(as.matrix(infRmDf[c(11:30)]), scale = TRUE, check_duplicates=FALSE, pca=TRUE, perplexity=100, theta=0.5, dims=2)
 
-ggplot(dfts, aes(x = date, y = dep)) +
-  geom_line() +
-  scale_x_date(name = "Date (239 days)", 
-               date_labels = "%d-%m-%Y", 
-               date_breaks = "2.5 weeks") +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+tsne_V1V2 = as.data.frame(tsne.pca$Y)
+
+tsne_original <- tsne_V1V2
+
+#kmeans - optimal groups -> 3 (Eigenvalue >1)
+fit_cluster_kmeans <-  kmeans(scale(tsne_V1V2), 4)
+tsne_original$cl_kmeans <-  factor(fit_cluster_kmeans$cluster)
+
+tsne_V1V2$cluster <- tsne_original$cl_kmeans
+
+ggplot(data=tsne_V1V2, aes(x=V1, y=V2, color=tsne_original$cl_kmeans)) + 
+  geom_point() + 
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Mental State Clusters\n", x = "Dim 1", y = "Dim 2", color = "Mental State Clusters\n")  
 
 ####################
-#Corrolation matrix
-
+#Corrolation matrix 20 mood types
 fa_cor <- cor(infRmDf[c(11:30)], method = "spearman")
 fa_cor <- sqrt(fa_cor^2) #try for normality
 fa_cor = as.data.frame(t(fa_cor))
@@ -67,96 +67,111 @@ fa_cor$groups <- NA
 fa_cor$RowNumber <- seq.int(nrow(fa_cor))
 
 ####################
-#Assumption Test  
-#cortest.bartlett(df[c(11:30)] , n=1476) #(Snedecor et al 1989)
-#out -> Bartlett's test is significant, x2(190) = 23547, p < .001 - (Cerny & Kaiser 1977, pp.43-47)
+#Assumption Test for FA/PCA  
+#cortest.bartlett(infRmDf[c(11:30)] , n=1476) #(Snedecor et al 1989)
+#out -> Bartlett's test is significant, x2(190) = 23514.21, p < .001 - (Cerny & Kaiser 1977, pp.43-47)
 # and therefore factor analysis is appropriate.
-#KMO(r = df[, c(11:30)]) #Overall MSA =  0.95 
-#fa.parallel(df[,c(11:30)],fm="pa") #quasi 3
-#alpha(df[c(11:30)], check.keys=TRUE) 
-#Cronbach alpha, >0.94 - (Yurdugül, H, 2008)
-#det(fa_cor)
-#which(fa_cor > 0.8)
-####################
+#KMO(r = infRmDf[, c(11:30)]) #Overall MSA =  0.94 
+#fa.parallel(infRmDf[,c(11:30)],fm="pa") #quasi 4
+#alpha(df[c(11:30)], check.keys=TRUE) #Cronbach alpha, >0.94 - (Yurdugül, H, 2008)
 
-#use EFA and kmeans confirm number of gruops
-#kmeans - optimal groups -3
-DataStandardised <- scale(infRmDf[11:30])
-DataStandardised <-data.matrix(DataStandardised)
-groups <- kmeans(DataStandardised, 3)
-clusplot(DataStandardised, groups$cluster)
-str(groups)
-
-fviz_cluster(groups, data = DataStandardised )  +
-  ggtitle("Kmeans Mental State CLusters") +
-  theme(plot.title = element_text(hjust = 0.5))   
- 
-#################################
-
-faGroups <- principal(df[c(11:30)], nfactors=3, rotate="oblimin", scores = TRUE)
-fa.groups <- print.psych(faGroups , cut = 0.4, sort = TRUE)
-fa.diagram(faGroups)
+# negitive loadings - high score on the factor is associated with a lower score on the item.
 
 #residuals view 
-residuals <- factor.residuals(fa_cor[c(1:20)], faGroups$loadings)
-residuals <- as.matrix(residuals[upper.tri(residuals)])
-large.resid <- abs(residuals) > 0.05
-sum(large.resid)/nrow(residuals)
-sqrt(mean(residuals^2))
-hist(residuals)
+#residuals <- factor.residuals(fa_cor[c(1:20)], faGroups$loadings)
+#residuals <- as.matrix(residuals[upper.tri(residuals)])
+#large.resid <- abs(residuals) > 0.05
+#sqrt(mean(residuals^2))
+#ggplot(residuals, aes(x=residuals))+ 
+#geom_histogram(color="black", fill="white", binwidth=.1)
 
-#Assign groups based on efaGroups
-fa_cor[fa_cor$RowNumber %in% c(16, 12, 20, 9, 17, 11, 4, 7, 19), "groups"] <- "Mood Positive"
-fa_cor[fa_cor$RowNumber %in% c(5, 10, 2, 15, 6, 8, 18), "groups"] <- "Mood Negitive"
-fa_cor[fa_cor$RowNumber %in% c(3, 14, 13,1), "groups"] <- "Mood Uncertain"
+Dist <- daisy(fa_cor[c(1:20)], metric = c("gower"), type=list())
+Dist <- as.matrix(Dist)
+heatmap(Dist, Rowv=TRUE, Colv="Rowv", symm = TRUE)
 
-#Eigenvalue >1
-plot(faGroups$values, type = "b")
-abline(h = 1, col="red", lty=5)
-legend("topright", legend=c("Eigenvalue = 1"),
-       col=c("red"), lty=5, cex=0.6)
+#PCA (rotated) to understand component groups
+#################################
+#Eigenvalue >1 (No.3, similar to parallel test)
+eign.df <- psych::principal(infRmDf[c(11:30)], nfactors=20)
+ggplot(, aes(x=fa_cor$RowNumber, y=eign.df$values)) + 
+  geom_line() +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  labs(title = "Eigenvalues > 1\n", x = "Factors", y = "Eigenvalues") +
+  geom_hline(yintercept = 1, color="blue")  +
+  scale_y_continuous(breaks=pretty_breaks(n=10)) +
+  geom_point()
 
+fa.pca <- psych::principal(infRmDf[c(11:30)], nfactors=4, rotate="oblimin", scores = TRUE)
+fa.groups <- print.psych(fa.pca , cut = 0.43, sort = TRUE)
+#fa.diagram(fa.pca)
 
+#Assign groups based on PCA routed / further PCA analaysys not done here
+#fa_cor[fa_cor$RowNumber %in% c(16, 20, 12, 9, 17, 11, 7, 4, 19), "groups"] <- "Mood Positive"
+#fa_cor[fa_cor$RowNumber %in% c(3, 14, 13, 1), "groups"] <- "Mood Uncertain"
+#fa_cor[fa_cor$RowNumber %in% c(15, 8, 18, 2), "groups"] <- "Mood Negitive"
+#fa_cor[fa_cor$RowNumber %in% c(6, 10, 5), "groups"] <- "Mood Worried"
+
+#mood status summarise
 #########################
+moods <- infRmDf
+# negative affect
+Mood_Positive <- c(26, 30,22, 19, 27, 21, 17, 14, 29)
+moods$mp <-  moods %>% select(Mood_Positive) %>% rowMeans(.,na.rm = TRUE)
 
-#PCA setup
-#mood <- df[c(2,3, 5,11:30)]
-#mood <- mood %>% group_by(date) %>% summarise_at(vars(phase:se_handle), mean) 
-#moodDaily <- mood[c(4:22)] 
+Mood_Uncertain <- c(13, 24, 23, 11)
+moods$mu <- moods %>% select(Mood_Uncertain) %>% rowMeans(.,na.rm = TRUE)
 
-moodfa_cor.pca <- prcomp(fa_cor[c(1:20)], center = TRUE) # for fviz_pca_biplot / includes data standardisation
-moodfa_cor.pca$x
-#ggbiplot::ggbiplot(moodfa_cor.pca, ellipse=TRUE, labels=rownames(fa_cor), groups=fa_cor$groups)  
+Mood_Negitive <- c(25, 28, 28, 12)
+moods$mn <- moods %>% select(Mood_Negitive) %>% rowMeans(.,na.rm = TRUE)
 
-#moodefa_cor.pca$rotation
+Mood_Worried <- c(16, 20, 15)
+moods$mw <- moods %>% select(Mood_Worried) %>% rowMeans(.,na.rm = TRUE)
 
-#par(mfrow=c(2,2))
- 
-#summary(moodfa_cor.pca)
+moods$moodsums <- rowSums(moods[c("mp", "mu", "mn", "mw")])
 
-#PCA Plot
-fviz_pca_biplot(moodfa_cor.pca, geom.ind = "point", pointshape = 21, 
-             pointsize = 2, 
-             fill.ind = fa_cor$groups,
-             col.ind = "black", 
-             palette = "jco", 
-             addEllipses = TRUE,
-             label = "var",
-             col.var = "black",
-             repel = TRUE,
-             legend.title = "Mental State") +
-  ggtitle("PCA plot - Participant's Mental State Over Test Period") +
-  theme(plot.title = element_text(hjust = 0.5))
+moods$groups <- as.numeric(tsne_original$cl_kmeans)
 
+#Group day numbers and mood means
+moods.dfa <- moods %>% group_by(dayno) %>% summarise(moodsums = mean(moodsums), groups = floor(mean(groups)))
+moods.dfa$moodsums_dfa = NA
 
-#########################
+fit_cluster_kmeans$size
 
-#split dataset into phases,
+# determine DFA, in a moving window of 20 days
+window <- 2
+for (i in seq(window, max(moods$dayno), 1)) {
 
+  # get the sliding window data
+  sliding.window <- subset(moods, dayno > (i - window) & dayno <= i)
+    
+  # moodsums
+  dfa.anal <- dfa(time.series = sliding.window$moodsums,
+                      npoints = 30,
+                      window.size.range = c(10, nrow(sliding.window)),
+                      do.plot = FALSE)
+  
+  fgn.est <- estimate(dfa.anal) 
 
-Mood.Positive <- mean(c(16, 12, 17, 20, 9, 4, 11, 7, 19, 18))
-Mood.Negitive <- mean(c(5, 6, 10, 2, 15, 8))
-Mood.Uncertain <- mean(c(3, 14, 13,1))
+  moods.dfa$moodsums_dfa[moods.dfa$dayno == i] <- fgn.est
+  }
 
+#assessment periods
+rects <- data.frame(xstart = c(1, 29,43,99,156), xend = c(28,42,98,155,238)) #(Kossakowski et al., 2017)
 
-
+ggplot(data = na.omit(moods.dfa$moods.dfa)) + 
+      geom_point() +
+      geom_rect(data=rects, aes( xmin=xstart, xmax=xend, ymin = -Inf, ymax = Inf), alpha =0.1) + 
+      geom_line(aes(x=moods.dfa$dayno, y=moods.dfa$moodsums_dfa, colour= factor(moods.dfa$groups),  group=1 ), ) +
+      ylab("DFA (21-day window)") + 
+      xlim(c(0, 250)) + 
+      theme(plot.title = element_text(hjust = 0.5)) +
+      labs(title = "Mental States over Test Period with phases (shadowed) \n", x = "Day Number", y = "21 day Window" )  +
+      scale_color_discrete(name = "Mental State Groups",  labels=c("Mood Positive", "Mood Uncertain", "Mood Negitive", "Mood Worried"))    +  
+      annotate("text", x = 15, y = 2.08, label = "Baseline") +  
+      annotate("text", x = 36, y = 2.06, label = "No Change") +
+      annotate("text", x = 70, y = 2.08, label = "Reduce AD") +
+      annotate("text", x = 125, y = 2.08, label = "Post test") +
+      annotate("text", x = 195, y = 2.08, label = "Follow up")
+  
+  
+   
